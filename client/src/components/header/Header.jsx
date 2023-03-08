5ae54fec6988863978cde6029c3b4aee8125f190
paulgreenwood67 <109767559+paulgreenwood67@users.noreply.github.com>
2023-03-08 14:16:34 +0000

    Update Header.jsx

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -4,10 +4,12 @@ import "./header.css";
 //function providing a loction for the main image (imported from a URL in in the associated CSS file)
 export default function Header() {
   return (
-    <div className="header">
-      <div>
-        <div className="headerImg"></div>
+  
+      <div className="header">
+        <div>
+        <img src={"https://www.mailtravel.co.uk/images/greek-islands-hero-1"} className="headerImg" alt="My Image" />
+        </div>
       </div>
-    </div>
+    
   );
 }

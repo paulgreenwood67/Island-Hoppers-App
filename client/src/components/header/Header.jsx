9868d4bf906060edfae3748b4e5adc0bb75b8c15
paulgreenwood67 <109767559+paulgreenwood67@users.noreply.github.com>
2023-03-09 12:42:10 +0000

    Update Header.jsx

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./header.css";
+import header from '../../images/header.webp';
+
 
 //function providing a loction for the main image (imported from a URL in in the associated CSS file)
 export default function Header() {

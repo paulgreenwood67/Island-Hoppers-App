55bc779c8dcafe7f13868bbb80a2d9d6ff1ccfca
paulgreenwood67 <109767559+paulgreenwood67@users.noreply.github.com>
2023-03-09 12:56:46 +0000

    Update Header.jsx

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import "./header.css";
 import header from '../../images/header.webp';
+import headeralt from '../../images/headeralt.webp';
+
+// Use headerImage in your code
 
 
 //function providing a loction for the main image (imported from a URL in in the associated CSS file)
 export default function Header() {
   return (
   
-     <div className="header">
+      <div className="header">
         <div>
-        <img src={header} className="headerImg" alt="My Image" />
+        <img src={header} className="headerImg hide1" alt="My Image" />
+        <img src={headeralt} className="headerImg hide2" alt="My Image" />
         </div>
       </div>
-    
     
   );
 }

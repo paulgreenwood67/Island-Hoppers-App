68566eaa68d0587cb07a6cbb6a812200babe1287
paulgreenwood67 <109767559+paulgreenwood67@users.noreply.github.com>
2023-03-11 19:44:01 +0000

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ import Settings from "./pages/settings/Settings";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
 import About from "./pages/about/About";
+import Blog from "./pages/blog/Blog";
+import Test from "./pages/test/Test";
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import NotFound404 from "./pages/notFound404/NotFound404";
 import { useContext } from "react";
@@ -15,7 +17,7 @@ import { Context } from "./context/Context";
 //function to manage the authentication of sign in
 function App() {
   const { user, isAdmin } = useContext(Context);
- 
+  console.log("admin", isAdmin);
   let adminUser;
   if (user === null) {
     adminUser = false;
@@ -43,6 +45,8 @@ function App() {
         />
         <Route exact path="/write" element={user ? <Write /> : <Login />} />
         <Route exact path="/about" element={<About />} />
+        <Route exact path="/blog" element={<Blog />} />
+        <Route exact path="/test" element={<Test />} />
       
         <Route exact path="*" element={<NotFound404 />} />
       </Routes>
